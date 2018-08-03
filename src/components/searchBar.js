ed566228f62c989d9b0310108987705f18877559
Words1223 <amp069597@gmail.com>
2018-08-02 20:51:08 -0600

    created recent posts and search bar components and used them in the home component

---
+++ src/components/searchBar.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class SearchBar extends Component {
+    render() {
+        return (
+            <form className="logo-main">
+                <input placeholder="Search DailySmarty"/>
+            </form>
+        )
+    }
+}
+
+export default SearchBar;
