f031ff11328c1bcb99095b0454df6cb817864b58
Words1223 <amp069597@gmail.com>
2018-08-02 18:21:06 -0600

    initial commit

---
+++ src/components/app.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Logo from './logo';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <div>
+          <Logo/>
+        </div>
+      </div>
+    );
+  }
+}
+export default App;
